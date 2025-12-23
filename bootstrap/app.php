7fcda8a418f8fe2ddb9c0ee40f96aab2f1b131c5
YG THOR <51053902+ygthor@users.noreply.github.com>
2025-12-23 09:13:50 +0800

    Add timezone setting in app.php
    Set the default timezone to Asia/Kuala_Lumpur.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,3 +2,5 @@
 require_once dirname(__FILE__) . '/../env.php';
 require_once dirname(__FILE__) . '/../includes/classes/mysql.class.php';
 require_once dirname(__FILE__) . '/helper.php';
+
+date_default_timezone_set('Asia/Kuala_Lumpur');
